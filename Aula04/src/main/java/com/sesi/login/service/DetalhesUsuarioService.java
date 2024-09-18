00c29450f224f7a25c70ce7367cb64c878cd8184
Boldrin1 <srzip74@gmail.com>
2024-09-18 11:54:52 -0300

    18/09/2024

--- Aula04/src/main/java/com/sesi/login/service/DetalhesUsuarioService.java
+++ Aula04/src/main/java/com/sesi/login/service/DetalhesUsuarioService.java
@@ -1,14 +1,29 @@
 package com.sesi.login.service;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 
+import com.sesi.login.model.Usuario;
+import com.sesi.login.repository.UsuarioRepository;
+
 public class DetalhesUsuarioService implements UserDetailsService{
 
+	private UsuarioRepository usuarioRepository;
+	
+	
 	@Override
 	public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
 		// TODO Auto-generated method stub
+		Usuario usuario = usuarioRepository.findByNomeUsuario(nomeUsuario);
+		
+		if(usuario == null) {
+			throw new UsernameNotFoundException("Usuario não encontrado");
+		}
+		
+	
+		
 		return null;
 	}
 
